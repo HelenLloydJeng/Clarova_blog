9dad6967a3580d9f759f4dde128114645e2d6c70
Helen LLoyd Jeng <you@example.com>
2025-06-06 15:53:30 +0100

    Fix post_list URL and view integration

--- blog/views.py
+++ blog/views.py
@@ -9,13 +9,16 @@ def home(request):
 
 # Create your views here.
 
+
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_create(request):
     if request.method == "POST":
