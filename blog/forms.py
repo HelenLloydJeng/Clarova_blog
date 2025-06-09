c1775b529ee5ae4ae3eb2c0d2cee23bcaf9a6e3c
Helen LLoyd Jeng <you@example.com>
2025-06-09 22:04:54 +0100

    Add Comment model and CommentForm

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,11 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']  # example fields
+        fields = ['title', 'content']
+from .models import Comment
+from django import forms
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
