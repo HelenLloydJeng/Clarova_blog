6d46ad8ab526fce0b3417d2fb1e4f4c119a4771f
Helen LLoyd Jeng <you@example.com>
2025-06-13 11:27:31 +0100

    remove trailing space in views py

--- users/views.py
+++ users/views.py
@@ -5,7 +5,6 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 
-# ✅ PROFILE VIEW
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
@@ -15,3 +14,4 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
+ 
