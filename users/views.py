323ee4594b78aa184458bb6903944eab50e0d04e
Helen LLoyd Jeng <you@example.com>
2025-06-13 09:25:09 +0100

    Use class-based signup view only

--- users/views.py
+++ users/views.py
@@ -1,25 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .forms import SignUpForm
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
 
-def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')  # uses Django auth view
-    else:
-        form = SignUpForm()
-    return render(request, 'registration/signup.html', {'form': form})
-
-
+# ✅ PROFILE VIEW
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
+
+@
+
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
