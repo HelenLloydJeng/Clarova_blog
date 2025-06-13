1a91a00632942633600a5e04a1aeded008c2e7ad
Helen LLoyd Jeng <you@example.com>
2025-06-13 09:02:38 +0100

    Fix duplicate signup path

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from .views import SignUpView
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
-    path('signup/', SignUpView.as_view(), name='signup'),
+
 ]
