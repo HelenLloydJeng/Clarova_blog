b1e4d15518eac4615b347ef03a6b16a5d3ad4382
Helen LLoyd Jeng <you@example.com>
2025-10-08 11:36:31 +0100

    feat(ui): add messages block; fix settings/env; update README and post list

--- my_project/settings.py
+++ my_project/settings.py
@@ -51,14 +51,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-nlrx-o9y%q+q$f9b1zaiveyxoypd_'
-    '*dx*46nwfo'
-    ')ns%u'
-)
+SECRET_KEY = env("SECRET_KEY", default="fallback-secret-key-for-dev")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY ", default="fallback-secret-key-for-dev")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=True)
